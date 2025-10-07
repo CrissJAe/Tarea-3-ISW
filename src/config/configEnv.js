e3ffde330ba73472a3160bc5419f3d1f792c3a46
Cristopher <andresaedo032@gmail.com>
2025-10-06 23:24:30 -0300

    fix: error al conectar con la base de datos

--- src/config/configEnv.js
+++ src/config/configEnv.js
@@ -1,7 +1,11 @@
 "use strict";
 import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+dotenv.config({ path: resolve(__dirname, ".env") });
 
 export const HOST = process.env.DB_HOST || process.env.HOST || "localhost";
 export const PORT = process.env.PORT || 3000;
