405220291f82e4367be0c9446e317171e3e1742c
Cristopher <andresaedo032@gmail.com>
2025-10-07 23:34:41 -0300

    add: Agregar validaciones a entidad usuario

---
+++ src/validations/user.validation.js
@@ -0,0 +1,44 @@
+"use strict";
+
+import Joi from "joi";
+
+export const userQueryValidation = Joi.object({
+    id: Joi.number()
+        .integer()
+        .positive()
+        .messages({
+            "number.base": "El ID debe ser un numero.",
+            "number.integer": "El ID debe ser un numero entero.",
+            "number.positive": "El ID debe ser positivo.",
+        }),
+});
+
+export const userBodyValidation = Joi.object({
+    email: Joi.string()
+        .min(6)
+        .max(255)
+        .pattern(/^[\w._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/)
+        .required()
+        .messages({
+            "string.min": "El email debe tener al menos 6 caracteres.",
+            "string.max": "El email debe tener como máximo 255 caracteres.",
+            "string.pattern.base": "El formato del email no es valido.",
+            "string.empty": "El email es obligatorio.",
+        }),
+    password: Joi.string()
+        .min(8)
+        .max(26)
+        .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+.,;:<>?{}\[\]~-]).+$/)
+        .required()
+        .messages({
+            "string.min": "La contraseña debe tener al menos 8 caracteres.",
+            "string.max": "La contraseña debe tener como máximo 26 caracteres.",
+            "string.pattern.base": "La contraseña debe incluir al menos una letra mayúscula, una minúscula, un número y un carácter especial.",
+            "string.empty": "La contraseña es obligatoria.",
+        }),
+})
+    .unknown(false)
+    .messages({
+        "object.unknown": "No se permiten campos adicionales.",
+    });
+
